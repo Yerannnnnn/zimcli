9fcf0623ad83dce1099d942759cd1874346db53e
nemo <jzyeran@gmail.com>
2024-02-28 19:23:37 +0800

    update logpath

--- src/main.cpp
+++ src/main.cpp
@@ -17,6 +17,8 @@ int qps = 1;
 int execution_time = 300; //default is 300s
 bool stopFlag = false;
 int debug = 0;
+std::string logpath = "/root/ZIMLogs";
+int logsize = 0;
 
 int main(int argc, char **argv)
 {
@@ -26,7 +28,10 @@ int main(int argc, char **argv)
 	app.add_option("--sender", sender, "sender's userID");
 	app.add_option("--receiver", receiver, "receiver's userID");
 	app.add_option("--qps", qps, "qps, 1~10. Default is 1.")->default_val(1);
+
 	app.add_option("--debug", debug, "debug or not. Default is 0.")->default_val(0);
+	app.add_option("--logpath", logpath, "logpath. Default is /root/ZIMLogs")->default_val("/root/ZIMLogs");
+	app.add_option("--logsize", logsize, "logsize. Default is 5242880(5M)")->default_val(0);
 	app.add_option("--execution-time", execution_time,
 		       "The execution time of this command line programs. 0~900s. Default is 300s.")
 		->default_val(300);
@@ -39,9 +44,13 @@ int main(int argc, char **argv)
 		execution_time = 900;
 	}
 
+	logpath = logpath + "/" + sender;
+
 	std::cout << "sender: " << sender << std::endl;
 	std::cout << "receiver: " << receiver << std::endl;
 	std::cout << "execution_time: " << execution_time << std::endl;
+	std::cout << "logpath: " << logpath << std::endl;
+	std::cout << "logsize: " << logsize << std::endl;
 	std::cout << "ZIM version: " << zim::ZIM::getVersion() << std::endl;
 
 	if (sender.empty() || receiver.empty()) {
@@ -56,6 +65,15 @@ int main(int argc, char **argv)
 	app_config.appSign = appsign;
 	zim_ = zim::ZIM::create(app_config);
 
+	zim::ZIMLogConfig log_config;
+
+	if (!logpath.empty()) {
+		log_config.logPath = "/root/ZIMLogs";
+	}
+	if (logsize != 0) {
+		log_config.logSize = logsize;
+	}
+	zim::ZIM::setLogConfig(log_config);
 	// login
 	std::cout << "Login ZIM..." << std::endl;
 	zim::ZIMUserInfo userInfo;
